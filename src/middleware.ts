0c29817564719af1dfc9435e193efad9fdc12ab3
Studio17-interiores <Info@studio17-interiores.com>
2026-07-08 16:46:27 +0200

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,5 @@
 import { NextResponse, type NextRequest } from "next/server";
-import { createServerClient } from "@supabase/ssr";
+import { createServerClient, type CookieOptions } from "@supabase/ssr";
 
 export async function middleware(request: NextRequest) {
   const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
@@ -19,12 +19,12 @@ export async function middleware(request: NextRequest) {
         get(name: string) {
           return request.cookies.get(name)?.value;
         },
-        set(name: string, value: string, options) {
+        set(name: string, value: string, options: CookieOptions) {
           request.cookies.set({ name, value, ...options });
           response = NextResponse.next({ request });
           response.cookies.set({ name, value, ...options });
         },
-        remove(name: string, options) {
+       remove(name: string, options: CookieOptions) {
           request.cookies.set({ name, value: "", ...options });
           response = NextResponse.next({ request });
           response.cookies.set({ name, value: "", ...options });
